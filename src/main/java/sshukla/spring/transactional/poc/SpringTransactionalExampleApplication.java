e3a4efcdfdbe0a1eec5041599471dbd87fa28d5f
Seemant Shukla <13106945+sshukla154@users.noreply.github.com>
2022-09-18 11:34:02 +0200

    Project Setup for Flight Service

---
+++ src/main/java/sshukla/spring/transactional/poc/SpringTransactionalExampleApplication.java
@@ -0,0 +1,13 @@
+package sshukla.spring.transactional.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTransactionalExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTransactionalExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sshukla/spring/transactional/poc/SpringTransactionalExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package sshukla.spring.transactional.poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTransactionalExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
